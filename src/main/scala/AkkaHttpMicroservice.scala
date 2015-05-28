64ee5d14c2842cabc267f9489a14d4829208cf03
Michael Pigg <mpigg@chariotsolutions.com>
2015-05-28 14:46:59 -0400

    Basic service

---
+++ src/main/scala/AkkaHttpMicroservice.scala
@@ -0,0 +1,56 @@
+import akka.actor.ActorSystem
+import akka.event.{LoggingAdapter, Logging}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.{ActorFlowMaterializer, FlowMaterializer}
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import java.io.IOException
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.math._
+import spray.json.DefaultJsonProtocol
+
+case class Order(number: String, customer: String, item: String)
+
+
+trait Protocols extends DefaultJsonProtocol {
+  implicit val orderFormat =jsonFormat2(Order.apply)
+}
+
+trait Service extends Protocols {
+  implicit val system: ActorSystem
+  implicit def executor: ExecutionContextExecutor
+  implicit val materializer: FlowMaterializer
+
+  def config: Config
+  val logger: LoggingAdapter
+
+  val routes = {
+    logRequestResult("order-microservice") {
+        get {
+          complete {
+            Vector(Order("138", "mpigg", "modelm"),
+              Order("139", "mpigg" , "omnikey"))
+          }
+        }
+    }
+  }
+}
+
+object AkkaHttpMicroservice extends App with Service {
+  override implicit val system = ActorSystem()
+  override implicit val executor = system.dispatcher
+  override implicit val materializer = ActorFlowMaterializer()
+
+  override val config = ConfigFactory.load()
+  override val logger = Logging(system, getClass)
+
+  Http().bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
+}
